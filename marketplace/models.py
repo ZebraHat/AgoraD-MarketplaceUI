4e80db47a7c031dca49fda45b3c5d44eded707df
Andrew Brown <andrew.brown@mst.edu>
2013-11-19 14:47:41 -0600

    initial marketplace models

--- marketplace/models.py
+++ marketplace/models.py
@@ -14,4 +14,58 @@
 # Django imports
 from django.utils.translation import ugettext_lazy as _
 from django.db import models
+from django import forms
 
+class Company(models.Model):
+	# contains many Sellers
+	name = models.CharField("company name", max_length=64)
+	#logo = models.ImageField("company logo", blank=True)
+	link = models.URLField("website link", blank=True)
+
+class User(models.Model):
+	name     = models.CharField("real name", max_length=64, primary_key=True)
+	email    = models.CharField("email address", max_length=128) # used as username to log in
+	password = forms.PasswordInput()
+
+	#class Meta:
+	#	abstract = True
+
+class Buyer(User):
+	user_id = models.ForeignKey(User, related_name='+')
+
+class Seller(User):
+	user_id = models.ForeignKey(User, related_name='+')
+	company = models.ForeignKey(Company)
+
+class Sellable(models.Model):
+	CATEGORIES = ( # TBD; should sellers be able to add their own?
+		('ANALYTICS', 'Analytics'),
+		('INTERESTS', 'Interests'),
+		('LOCATIONS', 'Locations')
+	)
+
+	# Metadata
+	seller = models.ForeignKey(Seller)
+	data_location = models.CharField("data location", max_length=64)
+	creation_date = models.DateField("creation date")
+
+	# List information
+	title = models.CharField("listing title", max_length=256)
+	description = models.CharField("listing description", max_length=1024) # probably make textfield instead
+	for_sale_date = models.DateField("date to start selling") # allow sellers to put data for sale at a future date
+	category = models.CharField("data category", max_length=1, choices=CATEGORIES)
+
+	def data_size(self): # computes data size if json blob/text dump, does something else if url to database
+		pass
+
+	def save(self, *args, **kwargs):
+		# Whenever a Sellable is modified, queue it up to save *after* any pending transfers of it are finished
+		# todo
+		super(Sellable, self).save(*args, **kwargs)
+
+class Transfer(models.Model):
+	seller = models.ForeignKey(Seller)
+	buyer  = models.ForeignKey(User, related_name='+') # allow buyers and sellers to buy, but only sellers to sell
+	data   = models.ForeignKey(Sellable)
+	transaction_queued = models.DateField("transfer initiated")
+	transaction_completed = models.DateField("transfer complete")
